309b858f7c1cbd09e50ce46c0673033a7ab6a335
arunchaudhary7455i <arunchaudhary7455i@gmail.com>
2026-05-04 09:02:13 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+public class BillingStrategy
+{
+    int usage,rate;
+    public static int FlatRate(int rate)
+    {
+        return rate;
+    }
+    public static int UsageBase(int usage,int rate)
+    {
+        return rate*usage;
+    }
+    public class InvoiceManagement
+    {
+        public static void Draft()
+        {
+            Console.WriteLine("Invoice Drafted Successfully");
+
+        }
+        public static void Finalized()
+        {
+            Console.WriteLine("Invoice Finalized Successfully");
+        }
+        public static void Paid()
+        {
+            Console.WriteLine("Invoice Paid Successfully");
+        }
+    }
+}
